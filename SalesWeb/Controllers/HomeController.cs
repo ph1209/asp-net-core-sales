63d583bb0d9e18bdf62f3b513e60679dbec78c49
Paulo Henrique <phlferreira1209@gmail.com>
2019-03-18 18:34:01 -0300

    Test Changes

--- SalesWeb/Controllers/HomeController.cs
+++ SalesWeb/Controllers/HomeController.cs
@@ -14,12 +14,14 @@ namespace SalesWeb.Controllers {
 
         public IActionResult About() {
             ViewData["Message"] = "Your application description page.";
+            
 
             return View();
         }
 
         public IActionResult Contact() {
             ViewData["Message"] = "Your contact page.";
+            ViewData["Name"] = "Paulo Henrique Lima";
 
             return View();
         }
