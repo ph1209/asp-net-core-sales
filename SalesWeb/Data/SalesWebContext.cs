1784fe1237b73f36b500018d0e5bc9cd127bbf13
Paulo Henrique <phlferreira1209@gmail.com>
2019-03-19 10:54:01 -0300

    Scaffolding Departments

---
+++ SalesWeb/Data/SalesWebContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesWeb.Models
+{
+    public class SalesWebContext : DbContext
+    {
+        public SalesWebContext (DbContextOptions<SalesWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWeb.Models.Department> Department { get; set; }
+    }
+}
--- SalesWeb/Startup.cs
+++ SalesWeb/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SalesWeb.Models;
 
 namespace SalesWeb {
     public class Startup {
@@ -28,6 +30,9 @@ namespace SalesWeb {
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+    services.AddDbContext<SalesWebContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("SalesWebContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
